140a42fb9233219aa5bfa75e646b13b93c1aa877
ROSENDO MENDOZA <rosendo.arturo@gmail.com>
2023-11-20 19:43:12 +0100

    Day 49: Add backend feature

---
+++ backend.py
@@ -0,0 +1,23 @@
+import os
+
+import openai
+
+class Chatbot:
+    def __init__(self):
+       openai.api_key = os.getenv("OPENAIAPIKEY")
+
+    def get_response(self, user_input):
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_input,
+            max_tokens=3000,
+            temperature=0.5
+        ).choices[0].text
+        return response
+
+if __name__ == "__main__":
+    chatbot = Chatbot()
+    response = chatbot.get_response("write a joke about birds")
+    print(response)
+
+
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import threading
 from PyQt6.QtWidgets import QMainWindow, QApplication, QTextEdit, QLineEdit, \
     QPushButton
+from backend import Chatbot
 import sys
 
 
 class ChatbotWindow (QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("My Chatbot GPT")
+        self.setWindowTitle("Smarty - Chatbot GPT")
         self.setMinimumSize(680, 400)
 
         # Add Chat Area Widget
@@ -17,17 +19,30 @@ class ChatbotWindow (QMainWindow):
         # Add Chat Input Widget
         self.input_chat = QLineEdit(self)
         self.input_chat.setGeometry(10, 320, 480, 25)
+        self.input_chat.returnPressed.connect(self.start_query)
 
         # Add Button
         self.button = QPushButton("Send", self)
         self.button.setGeometry(500, 320, 50, 25)
+        self.button.clicked.connect(self.start_query)
 
         self.show()
 
+    def start_query(self):
+        self.chatbot = Chatbot()
+        self.query = self.input_chat.text().strip()
+        self.chat_area.append(f"<p style='color:#333333'>User: {self.query}")
+        self.input_chat.clear()
+        print(self.query)
 
+        thread = threading.Thread(target=self.get_bot_response)
+        thread.start()
 
-class Chatbot:
-    pass
+    def get_bot_response(self):
+        response = self.chatbot.get_response(self.query)
+        print(response)
+        self.chat_area.append(f"<p style='color:#333333; "
+                              f"background-color:#E9E9E9'>Bot: {response}")
 
 
 app = QApplication(sys.argv)
